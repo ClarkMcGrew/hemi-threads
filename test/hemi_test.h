e3f0e5f7f8b314b6aa698f90e5be301b32c77635
Clark McGrew <clark.mcgrew@stonybrook.edu>
2024-05-10 09:12:31 -0400

    hemi_test.h : Honor the HEMI_CUDA_DISABLE definition

--- test/hemi_test.h
+++ test/hemi_test.h
@@ -1,4 +1,9 @@
 #include "gtest/gtest.h"
 
+#ifndef HEMI_CUDA_DISABLE
 #define ASSERT_SUCCESS(res) ASSERT_EQ(cudaSuccess, (res));
 #define ASSERT_FAILURE(res) ASSERT_NE(cudaSuccess, (res));
+#else
+#define ASSERT_SUCCESS(res)
+#define ASSERT_FAILURE(res)
+#endif
