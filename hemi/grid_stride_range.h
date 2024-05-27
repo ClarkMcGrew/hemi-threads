71c75cbb38c2925f27ff1e8664a4a2a36016ad70
Clark McGrew <clark.mcgrew@stonybrook.edu>
2024-05-27 10:36:30 -0400

    Remove step_range template from global namespace

--- hemi/grid_stride_range.h
+++ hemi/grid_stride_range.h
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
-// 
+//
 // "Hemi" CUDA Portable C/C++ Utilities
-// 
+//
 // Copyright 2012-2015 NVIDIA Corporation
 //
 // License: BSD License, see LICENSE file in Hemi home directory
@@ -9,7 +9,7 @@
 // The home for Hemi is https://github.com/harrism/hemi
 //
 ///////////////////////////////////////////////////////////////////////////////
-// Please see the file README.md (https://github.com/harrism/hemi/README.md) 
+// Please see the file README.md (https://github.com/harrism/hemi/README.md)
 // for full documentation and discussion.
 ///////////////////////////////////////////////////////////////////////////////
 #pragma once
@@ -20,20 +20,18 @@
 #include "hemi.h"
 #include "device_api.h"
 
-using namespace util::lang;
-
-// type alias to simplify typing...
-template<typename T>
-using step_range = typename range_proxy<T>::step_range_proxy;
-
 namespace hemi {
+
+        // type alias to simplify typing...
+        template<typename T>
+        using step_range = typename util::lang::range_proxy<T>::step_range_proxy;
 
 	template <typename T>
 	HEMI_DEV_CALLABLE_INLINE
 	step_range<T> grid_stride_range(T begin, T end) {
 	    begin += hemi::globalThreadIndex();
-	    return range(begin, end).step(hemi::globalThreadCount());
+	    return util::lang::range(begin, end).step(hemi::globalThreadCount());
 	}
-	
+
 }
 /////////////////////////////////////////////////////////////////
